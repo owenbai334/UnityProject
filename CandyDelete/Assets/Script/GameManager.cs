3864a002c18bfa47b17887a01ce1131156de20bf
owenbai334 <71922396+owenbai334@users.noreply.github.com>
2023-03-01 15:50:55 +0800

    31

--- CandyDelete/Assets/Script/GameManager.cs
+++ CandyDelete/Assets/Script/GameManager.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    //糖果的種類
+    public enum SweetType
+    {
+        EMPTY,
+        NORMAL,
+        BARRIER,
+        ROW_CLEAR,
+        COLUME_CLEAR,
+        RAINBOWCANDY,
+        COUNT//標記類型
+    } 
+    //糖果PREFAB的字典
+    private Dictionary<SweetType,GameObject> SweetPrefabDictionary;
+    
+    [System.Serializable]
+    public struct SweetPreab
+    {
+        public SweetType type;
+        public GameObject prefab;
+    }
+    public SweetPreab[] SweetPrefabs;
     private static GameManager _instance;
     public static GameManager Instance
     {
@@ -28,6 +48,14 @@ public class GameManager : MonoBehaviour
     //0.65 0.49
     void Start()
     {
+        SweetPrefabDictionary = new Dictionary<SweetType, GameObject>();
+        for (int i = 0; i < SweetPrefabs.Length; i++)
+        {
+            if(!SweetPrefabDictionary.ContainsKey(SweetPrefabs[i].type))
+            {
+                SweetPrefabDictionary.Add(SweetPrefabs[i].type,SweetPrefabs[i].prefab);
+            }
+        }
         for (int x = 0; x < GridColume; x++)
         {
             for (int y = 0; y < GridRow; y++)
